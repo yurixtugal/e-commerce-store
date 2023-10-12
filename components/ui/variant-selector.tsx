65b5777ce5b7bcf4f058e4391ae12bd4fcb01034
yurixtugal <yurixtugal12@gmail.com>
2023-10-12 08:56:22 -0500

    Fix error

--- components/ui/variant-selector.tsx
+++ components/ui/variant-selector.tsx
@@ -32,38 +32,39 @@ const VariantSelector = ({ data }: VariantProps) => {
               </label>
             </div>
             <div key={`option_${option}`} className="flex flex-col mb-4">
-          <label
-            className="flex flex-row flex-wrap gap-4"
-            key={`value_${option}}`}
-          >
-            {valuesByOption?.map((value) => {
-              optionSearchParams.set(optionNameLowerCase, value.name);
+              <label
+                className="flex flex-row flex-wrap gap-4"
+                key={`value_${option}}`}
+              >
+                {valuesByOption?.map((value) => {
+                  optionSearchParams.set(optionNameLowerCase, value.name);
 
-              return (
-                <button
-                  className="min-w-[70px] rounded-full border bg-neutral-100 px-2 py-1 text-md"
-                  key={`button_${value.value}`}
-                  onClick={() => {
-                    router.push(pathname + "?a=a");
-                  }}
-                >
-                  {option === "Color" ? (
-                    <div className="flex" key={`button_div_${value.value}`}>
-                      {" "}
-                      {value.name}{" "}
-                      <div
-                        className="mt-[1.5px]  ml-1 w-5 h-5 rounded-full border border-black"
-                        style={{ backgroundColor: value.value }} key={`button_div_2_${value.value}`}
-                      ></div>
-                    </div>
-                  ) : (
-                    value.value
-                  )}
-                </button>
-              );
-            })}
-          </label>
-        </div>
+                  return (
+                    <button
+                      className="min-w-[70px] rounded-full border bg-neutral-100 px-2 py-1 text-md"
+                      key={`button_${value.value}`}
+                      onClick={() => {
+                        router.push(pathname + "?a=a");
+                      }}
+                    >
+                      {option === "Color" ? (
+                        <div className="flex" key={`button_div_${value.value}`}>
+                          {" "}
+                          {value.name}{" "}
+                          <div
+                            className="mt-[1.5px]  ml-1 w-5 h-5 rounded-full border border-black"
+                            style={{ backgroundColor: value.value }}
+                            key={`button_div_2_${value.value}`}
+                          ></div>
+                        </div>
+                      ) : (
+                        value.value
+                      )}
+                    </button>
+                  );
+                })}
+              </label>
+            </div>
           </div>
         );
       })}
