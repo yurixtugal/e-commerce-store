20ee66d2d93c87e3ae7476b0c716561b9bb9c5f4
yurixtugal <yurixtugal12@gmail.com>
2023-10-13 04:10:18 -0500

    Fix warnings

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,13 +4,10 @@ import NavBar from "@/components/navigation/nav-bar-component";
 const StoreComponent = async ({ children }: { children: React.ReactNode }) => {
   const store = await getStore();
   return (
-    <html lang="en">
-      <body className="h-screen">
-        <NavBar store={store} />
-        <div className="flex flex-col h-full">{children}</div>
-        
-      </body>
-    </html>
+    <>
+      <NavBar store={store} />
+      <div className="flex flex-col h-full">{children}</div>
+    </>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default  function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning>
+      <body >
         {children}
       </body>
     </html>
